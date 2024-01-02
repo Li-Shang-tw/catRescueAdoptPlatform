386d2b2d3106b7b71da763668773d4ee14028d3c
root <root@LAPTOP-LQRHI4AS>
2024-01-02 17:18:27 +0800

    feat:router link to detail page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import Chip from "@mui/material/Chip";
+import { Link } from "react-router-dom";
 
 export default function Card({ currentRescueCats }) {
   const cardsList = currentRescueCats.map((item) => (
@@ -6,33 +7,37 @@ export default function Card({ currentRescueCats }) {
       key={item.id}
       className=" shadow  rounded-xl duration-100 hover:scale-110  hover:shadow-lg"
     >
-      <div className="relative">
-        <img
-          className="rounded-md rounded-b-3xl mb-2.5 "
-          src={
-            item.images[0] ? item.images[0] : "src/assets/imgs/rescuephoto2.jpg"
-          }
-          alt="rescueImg"
-        />
-      </div>
-      <div className="mx-1.5">
-        <h3 className="mb-px font-bold text-lg">
-          {item.name ? item.name : "待救援的貓貓"}
-        </h3>
-        <div>
-          <span className="mb-px mr-2.5">{item.ageCategory}</span>
-          <span className="mb-px">{item.location}</span>
+      <Link to={`${item.id}`}>
+        <div className="relative">
+          <img
+            className="rounded-md rounded-b-3xl mb-2.5 "
+            src={
+              item.images[0]
+                ? item.images[0]
+                : "src/assets/imgs/rescuephoto2.jpg"
+            }
+            alt="rescueImg"
+          />
         </div>
-        <div className="flex items-baseline mt-1">
-          <div className="mr-1">
-            {item.riskLevel === 3 && <Chip label="高" color="error" />}
-            {item.riskLevel === 2 && <Chip label="中" color="warning" />}
-            {item.riskLevel === 1 && <Chip label="低" color="success" />}
-            {item.riskLevel === "" && <Chip label="未分類" />}
+        <div className="mx-1.5">
+          <h3 className="mb-px font-bold text-lg">
+            {item.name ? item.name : "待救援的貓貓"}
+          </h3>
+          <div>
+            <span className="mb-px mr-2.5">{item.ageCategory}</span>
+            <span className="mb-px">{item.location}</span>
           </div>
-          <p className="mb-5">{item.symptoms}</p>
+          <div className="flex items-baseline mt-1">
+            <div className="mr-1">
+              {item.riskLevel === 3 && <Chip label="高" color="error" />}
+              {item.riskLevel === 2 && <Chip label="中" color="warning" />}
+              {item.riskLevel === 1 && <Chip label="低" color="success" />}
+              {item.riskLevel === "" && <Chip label="未分類" />}
+            </div>
+            <p className="mb-5">{item.symptoms}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   ));
   return (
