0e00b52d3e2cfd03049c4a0c8c1496ce4a0b49a3
root <root@LAPTOP-LQRHI4AS>
2024-01-03 11:39:05 +0800

    fix:install react hook form again

--- src/page/RescueDetail.jsx
+++ src/page/RescueDetail.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+//先引入資料
+// import rescurData from "../assets/rescueData.json";
 
 export default function RescueDetail() {
   const { id } = useParams();
+  // const rescueProject = rescurData.find((item) => item.id === id);
+  // console.log(rescueProject);
   return (
     <div>
       <h1>RescueDetail{id}</h1>
