2ad253dcd9399a69a7d2f16fbb17dae241653b8e
root <root@LAPTOP-LQRHI4AS>
2024-01-03 13:20:49 +0800

    feat:rescue card detail and detail page layout

---
+++ src/components/CardDetail.jsx
@@ -0,0 +1,36 @@
+import Chip from "@mui/material/Chip";
+import Button from "@mui/material/Button";
+
+export default function RescueDetail({ rescueProject }) {
+  console.log(rescueProject);
+  return (
+    <div className="shadow  rounded-xl px-3 py-4">
+      <h2 className="text-2xl font-bold  mb-2">{rescueProject.name}</h2>
+      <div className="flex justify-start mb-3">
+        <h4>{rescueProject.location}</h4>
+        <h4>{rescueProject.ageCategory}</h4>
+      </div>
+      <div>
+        <div className="flex items-baseline mb-3">
+          <div className="mr-1">
+            {rescueProject.riskLevel === 3 && <Chip label="高" color="error" />}
+            {rescueProject.riskLevel === 2 && (
+              <Chip label="中" color="warning" />
+            )}
+            {rescueProject.riskLevel === 1 && (
+              <Chip label="低" color="success" />
+            )}
+            {rescueProject.riskLevel === "" && <Chip label="未分類" />}
+          </div>
+          <p className="mb-5">{rescueProject.symptoms}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>{rescueProject.ctaRescue}</p>
+          <Button variant="contained" color="warning">
+            斗內
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/page/RescueDetail.jsx
+++ src/page/RescueDetail.jsx
@@ -1,14 +1,24 @@
 import { useParams } from "react-router-dom";
 //先引入資料
-// import rescurData from "../assets/rescueData.json";
+import rescurData from "../assets/rescueData.json";
 
+//載入元件
+import Carousel from "../components/Carousel";
+import CardDetail from "../components/CardDetail";
 export default function RescueDetail() {
+  //取得id,並轉成數字
   const { id } = useParams();
-  // const rescueProject = rescurData.find((item) => item.id === id);
-  // console.log(rescueProject);
+
+  //找出符合路徑id的資料
+  //id是字串，要轉成數字
+  const rescueProject = rescurData.find((item) => item.id === parseInt(id));
+
   return (
     <div>
-      <h1>RescueDetail{id}</h1>
+      <Carousel />
+      <div className="flex justify-between">
+        <CardDetail rescueProject={rescueProject} />
+      </div>
     </div>
   );
 }
