b8b363516d41f8602ddad0d82e2c91af8f933038
root <root@LAPTOP-LQRHI4AS>
2024-01-07 10:20:29 +0800

    refactor:sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -5,25 +6,30 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
 export default function Sort({ sort }) {
+  const [option, setOption] = useState("timeDes");
   function handleOptionChange(e) {
     sort(e.target.value);
+    setOption(e.target.value);
   }
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value="10"
-          label="Age"
-          onChange={handleOptionChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <div className="flex justify-end mb-2">
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl>
+          <InputLabel id="sort-select-label"> 排序</InputLabel>
+          <Select
+            labelId="sort-select-label"
+            id="sort-select"
+            value={option}
+            label="Sort"
+            onChange={handleOptionChange}
+          >
+            <MenuItem value={"timeDes"}>新>舊(時間)</MenuItem>
+            <MenuItem value={"timeAsc"}>舊>新(時間)</MenuItem>
+            <MenuItem value={"riskDes"}>高>低(危險度)</MenuItem>
+            <MenuItem value={"riskAsc"}>低>高(危險度)</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
   );
 }
