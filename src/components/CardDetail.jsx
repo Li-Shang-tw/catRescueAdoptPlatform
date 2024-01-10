83053f85ff31cdb19f7ce06b61438f647cd7af2a
root <root@LAPTOP-LQRHI4AS>
2024-01-10 16:10:41 +0800

    refactor: use interpreter function to interpret locationName and AgeCatergory

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,5 +1,8 @@
 import Chip from "@mui/material/Chip";
 import Button from "@mui/material/Button";
+//引用composable
+import { getLoactionName } from "../composable/getLocationName";
+import interpretAgeCategory from "../composable/interpretAgeCatergory";
 
 export default function RescueDetail({ rescueProject }) {
   console.log(rescueProject);
@@ -9,8 +12,8 @@ export default function RescueDetail({ rescueProject }) {
         {rescueProject.name ? rescueProject.name : "待救援的貓貓"}
       </h2>
       <div className="flex justify-start mb-3">
-        <h4>{rescueProject.location}</h4>
-        <h4>{rescueProject.ageCategory}</h4>
+        <h4 className="mr-4">{getLoactionName(rescueProject.location)}</h4>
+        <h4>{interpretAgeCategory(rescueProject.ageCategory)}</h4>
       </div>
       <div>
         <div className="flex items-baseline mb-3">
