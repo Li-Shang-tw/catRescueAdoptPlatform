3318ef72b8ee6394ac99b092eb9b3bb340b217ca
root <root@LAPTOP-LQRHI4AS>
2024-01-16 14:24:24 +0800

    feat: use useSearchParams to get q

--- src/page/Achievement.jsx
+++ src/page/Achievement.jsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Achievement() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
+
   return (
     <div>
       <h2>成就</h2>
