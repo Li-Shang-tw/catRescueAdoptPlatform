ee7ba4f5ed19211a49042556e13ac8bfba8b2513
root <root@LAPTOP-LQRHI4AS>
2024-01-16 20:04:20 +0800

    feat: only show update btn while current user's project

--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -1,15 +1,19 @@
 import ProgressBar from "./ProgressBar";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import ModalSet from "./ModalSet";
 import EditBtn from "./EditBtn";
 import FormForUpdateProgress from "./FormForUpdateProgress";
 import FormTransterToAdopt from "./FormTransterToAdopt";
 import Chip from "@mui/material/Chip";
+
+import { CurrentUserContext } from "../context/CurrentUserContext";
 export default function ProgressCard({ rescueProject, handleUpdatRescueCat }) {
   //取得目前金額與目標金額與計算進度
   const currentAmount = rescueProject.currentAmount;
   const targetAmount = rescueProject.targetAmount;
   const progress = (currentAmount / targetAmount) * 100;
+  //取得current user的資料
+  const { currentUser } = useContext(CurrentUserContext);
 
   return (
     <div className="bg-white p-4">
@@ -28,19 +32,20 @@ export default function ProgressCard({ rescueProject, handleUpdatRescueCat }) {
           <p className="text-xl font-bold"> NT$ {currentAmount}</p>
         </div>
         <div>
-          {rescueProject.state === "1" && (
-            <div className="flex justify-end">
-              <ModalSet
-                btn={<EditBtn>更新進度</EditBtn>}
-                form={
-                  <FormForUpdateProgress
-                    rescueProject={rescueProject}
-                    handleUpdatRescueCat={handleUpdatRescueCat}
-                  />
-                }
-              />
-            </div>
-          )}
+          {rescueProject.state === "1" &&
+            rescueProject.rescuerId === currentUser.id && (
+              <div className="flex justify-end">
+                <ModalSet
+                  btn={<EditBtn>更新進度</EditBtn>}
+                  form={
+                    <FormForUpdateProgress
+                      rescueProject={rescueProject}
+                      handleUpdatRescueCat={handleUpdatRescueCat}
+                    />
+                  }
+                />
+              </div>
+            )}
           {rescueProject.state === "2" && (
             <ModalSet
               btn={<EditBtn>轉成收養專站</EditBtn>}
