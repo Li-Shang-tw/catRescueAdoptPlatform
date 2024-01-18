42ea75d2d6cdf474fb4900b8eaff330cc90b309d
root <root@LAPTOP-LQRHI4AS>
2024-01-18 20:10:38 +0800

    feat: show feedback message while there is no data

--- src/components/AdoptingCardList.jsx
+++ src/components/AdoptingCardList.jsx
@@ -3,41 +3,48 @@ import { getLoactionName } from "../composable/getLocationName";
 import interpretAgeCatergory from "../composable/interpretAgeCatergory";
 
 export default function AdoptingCardList({ currentCats }) {
-  const cardsList = currentCats.map((item) => (
-    <li
-      key={item.id}
-      className=" shadow  rounded-xl duration-100 hover:scale-110  hover:shadow-lg bg-white"
-    >
-      <Link to={`${item.id}`}>
-        <div className="relative">
-          <img
-            className="rounded-md rounded-b-3xl mb-2.5 "
-            src={
-              item.images[0]
-                ? item.images[0]
-                : "src/assets/imgs/rescuephoto2.jpg"
-            }
-            alt="rescueImg"
-          />
-        </div>
-        <div className="mx-1.5">
-          <h3 className="mb-px font-bold text-lg">
-            {item.name ? item.name : "可愛的貓貓"}
-          </h3>
-          <div>
-            <span className="mb-px mr-2.5">
-              {interpretAgeCatergory(item.ageCategory)}
-            </span>
+  let cardsList;
+  //如果尚未有資料的話
+  if (currentCats === "Not found") {
+    cardsList = <p className="text-lg font-semibold"> 暫無要領養的貓貓</p>;
+  } else {
+    cardsList = currentCats.map((item) => (
+      <li
+        key={item.id}
+        className=" shadow  rounded-xl duration-100 hover:scale-110  hover:shadow-lg bg-white"
+      >
+        <Link to={`${item.id}`}>
+          <div className="relative">
+            <img
+              className="rounded-md rounded-b-3xl mb-2.5 "
+              src={
+                item.images[0]
+                  ? item.images[0]
+                  : "src/assets/imgs/rescuephoto2.jpg"
+              }
+              alt="rescueImg"
+            />
+          </div>
+          <div className="mx-1.5">
+            <h3 className="mb-px font-bold text-lg">
+              {item.name ? item.name : "可愛的貓貓"}
+            </h3>
+            <div>
+              <span className="mb-px mr-2.5">
+                {interpretAgeCatergory(item.ageCategory)}
+              </span>
 
-            <span className="mb-px">{item.breed}</span>
+              <span className="mb-px">{item.breed}</span>
+            </div>
+            <div className="flex items-baseline mt-1">
+              <p>{getLoactionName(item.location)}</p>
+            </div>
           </div>
-          <div className="flex items-baseline mt-1">
-            <p>{getLoactionName(item.location)}</p>
-          </div>
-        </div>
-      </Link>
-    </li>
-  ));
+        </Link>
+      </li>
+    ));
+  }
+
   return (
     <>
       <ul className="grid  gap-4 lg:grid-cols-4  md:grid-cols-3  sm:grid-cols-2 mb-4">
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
+export default function loading() {
+  <Stack sx={{ color: "grey.500" }} spacing={2} direction="row">
+    <CircularProgress color="secondary" />
+    <CircularProgress color="success" />
+    <CircularProgress color="inherit" />
+  </Stack>;
+}
