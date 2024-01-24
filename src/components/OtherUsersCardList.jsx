9ead21bb18bb554bf0b6cc72aa5cdf05cb1e5873
root <root@LAPTOP-LQRHI4AS>
2024-01-24 17:55:20 +0800

    feat: build other user oview

---
+++ src/components/OtherUsersCardList.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+import Loading from "./Loading";
+import UserCard from "./UserCard";
+
+export default function OtherUsersCardList({ currentCats }) {
+  console.log(currentCats);
+  let cardsList;
+  //如果尚未有資料的話
+  if (currentCats === null) {
+    return <Loading />;
+  } else if (!currentCats) {
+    return <p>暫時沒有用戶資料</p>;
+  }
+
+  cardsList = currentCats.map((item) => (
+    <li
+      key={item.id}
+      className=" shadow  rounded-xl duration-100 hover:scale-110  hover:shadow-lg bg-white"
+    >
+      <Link to={`/adopt/${item.id}`}>
+        <UserCard currentUser={item} style="flex-col" />
+      </Link>
+    </li>
+  ));
+
+  return (
+    <>
+      <ul className="grid  gap-4 lg:grid-cols-4  md:grid-cols-3  sm:grid-cols-2 mb-4">
+        {cardsList}
+      </ul>
+    </>
+  );
+}
--- src/page/OtherUsers.jsx
+++ src/page/OtherUsers.jsx
@@ -1,6 +1,9 @@
 import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getOtherUsersAPI } from "../callAPI.js";
+import OtherUsersCardList from "../components/OtherUsersCardList.jsx";
+
+import Overview from "./Overview.jsx";
 export default function OtherUsers() {
   const [otherUsers, setOtherUsers] = useState(null);
   const [searchParams] = useSearchParams();
@@ -13,9 +16,50 @@ export default function OtherUsers() {
     fetchData();
   }, []);
 
+  //current page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  //=========pagination=========
+  function handlePageChange(event, page) {
+    setCurrentPage(page);
+  }
+
+  //顯示當前頁面的資料
+  let currentOtherUsers;
+  let totalPages;
+  if (otherUsers) {
+    currentOtherUsers = otherUsers.slice(
+      (currentPage - 1) * 8,
+      currentPage * 8
+    );
+    //所有頁數
+    totalPages = Math.ceil(otherUsers.length / 8);
+  }
+
+  //======sort=======
+  function handleSort() {
+    console.log("sort");
+  }
+
   return (
-    <div>
-      <h1>otherUsers{role}</h1>
-    </div>
+    <>
+      <div className="container mx-auto px-4">
+        <Overview
+          currentCats={currentOtherUsers}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+          handleSort={handleSort}
+          Card={OtherUsersCardList}
+        >
+          <div>
+            <h2>
+              {(role === "rescuer" && "救援者總攬") ||
+                (role === "adopter" && "收養者總覽")}
+            </h2>
+          </div>
+        </Overview>
+      </div>
+    </>
   );
 }
