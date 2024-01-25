05641cd66643fa0bf89689db9325857727854067
root <root@LAPTOP-LQRHI4AS>
2024-01-25 10:52:42 +0800

    feat: get specific user data through api

--- src/page/OtherUserDetail.jsx
+++ src/page/OtherUserDetail.jsx
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUserAPI } from "../callAPI.js";
 export default function OtherUserDetail() {
   const { id } = useParams();
+  const [otherUser, setOtherUser] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getUserAPI(id);
+      console.log(data);
+      setOtherUser(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h2>其他用戶細節{id}</h2>
+      <p>{otherUser && otherUser.name}</p>
     </>
   );
 }
