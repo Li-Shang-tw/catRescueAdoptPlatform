c45f610f76aae342614fb62a45ac46c8d0ebb3ca
root <root@LAPTOP-LQRHI4AS>
2024-01-30 14:29:14 +0800

    feat: build adopt function which update cat and user data in api and state

--- src/components/AdoptCardDetail.jsx
+++ src/components/AdoptCardDetail.jsx
@@ -3,16 +3,59 @@ import Chip from "@mui/material/Chip";
 //引用composable
 import { getLoactionName } from "../composable/getLocationName";
 import interpretAgeCategory from "../composable/interpretAgeCatergory";
+//引用api
+import { putCatAPI, putUserAPI } from "../callAPI";
+//取的當前使用者
+import { useContext } from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext";
+
 //引用元件
 import ModalSet from "./ModalSet";
 
 import EditBtn from "./EditBtn";
 
-export default function AdoptCardDetail({
-  adoptProject,
-  updateAdoptProject,
-  currentUser,
-}) {
+export default function AdoptCardDetail({ adoptProject, updateAdoptProject }) {
+  //取得當前用戶
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  function AdoptBtn(id) {
+    if (
+      adoptProject.requestingUsers &&
+      adoptProject.requestingUsers.includes(currentUser.id)
+    ) {
+      return (
+        <div>
+          <Chip label="已送出認養申請" variant="outlined" color="success" />
+        </div>
+      );
+    } else {
+      return (
+        <div onClick={() => requestAdopt(adoptProject.id)}>
+          <EditBtn>認養</EditBtn>
+        </div>
+      );
+    }
+  }
+  async function requestAdopt(id) {
+    //在cat資料上的requestAdopt加入currentUser的id
+    await putCatAPI(id, {
+      requestingUsers: [...adoptProject.requestingUsers, currentUser.id],
+    });
+    //更新adoptProject
+    updateAdoptProject({
+      requestingUsers: [...adoptProject.requestingUsers, currentUser.id],
+    });
+    //在user資料上的requestAdopt加入cat的id
+    await putUserAPI(currentUser.id, {
+      requestingProject: [...currentUser.requestingProject, id],
+    });
+    //更新currentUser
+    setCurrentUser({
+      ...currentUser,
+      requestingProject: [...currentUser.requestingProject, id],
+    });
+
+    alert("已送出認養申請");
+  }
   return (
     <div className="shadow  rounded-xl px-3 py-4 bg-white">
       <div className="flex justify-between">
@@ -24,7 +67,7 @@ export default function AdoptCardDetail({
         )}
         {currentUser &&
           adoptProject.rescuerId !== currentUser.id &&
-          currentUser.role === "adopter" && <EditBtn>認養</EditBtn>}
+          currentUser.role === "adopter" && <AdoptBtn id={adoptProject.id} />}
       </div>
       <div className="flex justify-start mb-3">
         <h4 className="mr-4">{getLoactionName(adoptProject.location)}</h4>
