f8a7f81a4b6570fa85b4da392ad0704da35c8eb9
root <root@LAPTOP-LQRHI4AS>
2024-02-05 00:56:15 +0800

    style: reset card style

--- src/components/AdoptingCardList.jsx
+++ src/components/AdoptingCardList.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { getLoactionName } from "../composable/getLocationName";
 import interpretAgeCatergory from "../composable/interpretAgeCatergory";
 import Loading from "./Loading";
+import Avatar from "@mui/material/Avatar";
 
 export default function AdoptingCardList({ currentCats }) {
   let cardsList;
@@ -15,30 +16,35 @@ export default function AdoptingCardList({ currentCats }) {
   cardsList = currentCats.map((item) => (
     <li
       key={item.id}
-      className=" shadow  rounded-xl duration-100 hover:scale-110  hover:shadow-lg bg-white"
+      className=" border-4 border-black rounded-xl duration-100 hover:scale-110   bg-white"
     >
       <Link to={`/adopt/${item.id}`}>
-        <div className="relative">
+        <div className="flex flex-col items-center ">
+          {/* <Avatar
+            alt="貓貓"
+            src="/src/assets/imgs/rescuephoto2.jpg"
+            sx={{ width: 250, height: 250 }}
+          /> */}
           <img
-            className="rounded-md rounded-b-3xl mb-2.5 "
-            //先將圖片都設成預設
+            className=" mb-2.5 rounded-b-sm"
             src="/src/assets/imgs/rescuephoto2.jpg"
             alt="rescueImg"
           />
-        </div>
-        <div className="mx-1.5">
-          <h3 className="mb-px font-bold text-lg">
-            {item.name ? item.name : "可愛的貓貓"}
-          </h3>
-          <div>
-            <span className="mb-px mr-2.5">
-              {interpretAgeCatergory(item.ageCategory)}
-            </span>
 
-            <span className="mb-px">{item.breed}</span>
-          </div>
-          <div className="flex items-baseline mt-1">
-            <p>{getLoactionName(item.location)}</p>
+          <div className="my-5 flex flex-col items-center">
+            <h3 className="mb-px font-bold text-2xl">
+              {item.name ? item.name : "可愛的貓貓"}
+            </h3>
+            <div>
+              <span className="mb-px mr-2.5">
+                {interpretAgeCatergory(item.ageCategory)}
+              </span>
+
+              <span className="mb-px">{item.breed}</span>
+            </div>
+            <div className="flex items-baseline mt-1">
+              <p>{getLoactionName(item.location)}</p>
+            </div>
           </div>
         </div>
       </Link>
