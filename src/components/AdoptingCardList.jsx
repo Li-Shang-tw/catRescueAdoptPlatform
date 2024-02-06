293200ee3fd4004451308c473b05f8612184ba68
root <root@LAPTOP-LQRHI4AS>
2024-02-06 12:08:41 +0800

    refactor: create catCard

--- src/components/AdoptingCardList.jsx
+++ src/components/AdoptingCardList.jsx
@@ -1,8 +1,5 @@
-import { Link } from "react-router-dom";
-import { getLoactionName } from "../composable/getLocationName";
-import interpretAgeCatergory from "../composable/interpretAgeCatergory";
 import Loading from "./Loading";
-import Avatar from "@mui/material/Avatar";
+import CatCard from "./CatCard";
 
 export default function AdoptingCardList({ currentCats }) {
   let cardsList;
@@ -14,40 +11,8 @@ export default function AdoptingCardList({ currentCats }) {
   }
 
   cardsList = currentCats.map((item) => (
-    <li
-      key={item.id}
-      className=" border-4 border-black rounded-xl duration-100 hover:scale-110   bg-white"
-    >
-      <Link to={`/adopt/${item.id}`}>
-        <div className="flex flex-col items-center ">
-          {/* <Avatar
-            alt="貓貓"
-            src="/src/assets/imgs/rescuephoto2.jpg"
-            sx={{ width: 250, height: 250 }}
-          /> */}
-          <img
-            className=" mb-2.5 rounded-b-sm"
-            src="/src/assets/imgs/rescuephoto2.jpg"
-            alt="rescueImg"
-          />
-
-          <div className="my-5 flex flex-col items-center">
-            <h3 className="mb-px font-bold text-2xl">
-              {item.name ? item.name : "可愛的貓貓"}
-            </h3>
-            <div>
-              <span className="mb-px mr-2.5">
-                {interpretAgeCatergory(item.ageCategory)}
-              </span>
-
-              <span className="mb-px">{item.breed}</span>
-            </div>
-            <div className="flex items-baseline mt-1">
-              <p>{getLoactionName(item.location)}</p>
-            </div>
-          </div>
-        </div>
-      </Link>
+    <li key={item.id}>
+      <CatCard item={item} type="adopt" />
     </li>
   ));
 
---
+++ src/components/CatCard.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { getLoactionName } from "../composable/getLocationName";
+import interpretAgeCatergory from "../composable/interpretAgeCatergory";
+import Chip from "@mui/material/Chip";
+export default function Card({ item, type }) {
+  return (
+    <div
+      key={item.id}
+      className=" border-4 border-black rounded-xl duration-100 hover:scale-110   bg-white"
+    >
+      <Link to={`/${type}/${item.id}`}>
+        <div className="flex flex-col items-center ">
+          <img
+            className=" mb-2.5 rounded-b-sm"
+            src="/src/assets/imgs/rescuephoto2.jpg"
+            alt="rescueImg"
+          />
+
+          <div className="my-5 flex flex-col items-center">
+            <h3 className="mb-px font-bold text-2xl">
+              {item.name ? item.name : "可愛的貓貓"}
+            </h3>
+            <div>
+              <span className="mb-px mr-2.5">
+                {interpretAgeCatergory(item.ageCategory)}
+              </span>
+              {type === "adopt" ? (
+                <span className="mb-px">{item.breed}</span>
+              ) : (
+                <span>{getLoactionName(item.location)}</span>
+              )}
+            </div>
+            {type === "adopt" && (
+              <div className="flex items-baseline mt-1">
+                <p>{getLoactionName(item.location)}</p>
+              </div>
+            )}
+
+            {type === "rescue" && (
+              <div className="flex items-baseline mt-1">
+                <div className="mr-1">
+                  {item.riskLevel === "3" && <Chip label="高" color="error" />}
+                  {item.riskLevel === "2" && (
+                    <Chip label="中" color="warning" />
+                  )}
+                  {item.riskLevel === "1" && (
+                    <Chip label="低" color="success" />
+                  )}
+                  {item.riskLevel === "" && <Chip label="未分類" />}
+                </div>
+                <p className="mb-5">{item.symptoms}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
