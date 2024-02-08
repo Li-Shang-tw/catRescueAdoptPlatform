1e4d12324a84983b765f2008d5b6fe38ec034c00
root <root@LAPTOP-LQRHI4AS>
2024-02-08 13:45:50 +0800

    style: apply card outline in profile card

--- src/components/CardInUserPage.jsx
+++ src/components/CardInUserPage.jsx
@@ -1,11 +1,12 @@
+import CardOutline from "./CardOutline";
 export default function CardInUserPage({ title, children }) {
   return (
     <>
-      <div className="flex justify-center font-medium mb-10">
-        <div className="w-1/2 border-2 px-10 py-4 rounded-xl bg-white">
+      <div className="flex justify-center font-medium pb-10">
+        <CardOutline style="w-1/2 px-5 py-4">
           <h3 className="text-xl font-bold mb-5">{title}</h3>
           {children}
-        </div>
+        </CardOutline>
       </div>
     </>
   );
