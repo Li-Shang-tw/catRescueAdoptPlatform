fc6e59ee49c01ecd48553a18dc568e43c6ef1c4f
root <root@LAPTOP-LQRHI4AS>
2024-02-15 21:26:53 +0800

    feat: make header fixed and change background color while scroll down

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,24 +1,41 @@
+import { useState, useContext, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+//----mui
 import Avatar from "@mui/material/Avatar";
-
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
+//----context
 import { CurrentUserContext } from "../context/CurrentUserContext";
-import { useContext } from "react";
+//---元件---
 import CreateBtn from "./CreateBtn";
 import FormForRescue from "./FormForRescue";
 import FormForAdopt from "./FormForAdopt";
 import ModalSet from "./ModalSet";
 import EditBtn from "./EditBtn";
-
 import DrawerMenu from "./DrawerMenu";
 
 export default function Navigator() {
   //---取得登入者資料-----
   const userData = useContext(CurrentUserContext);
   const { currentUser } = userData;
+  //----處理滾動事件---------
+  const [onTop, setOnTop] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      //當滾動到最上方時，OnTop為true，否則為false
+      if (window.scrollY === 0) {
+        setOnTop(true);
+      } else {
+        setOnTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    //移除事件監聽器
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   //取得用戶名稱與路徑id
   function getUserNameLastWord() {
@@ -39,96 +56,107 @@ export default function Navigator() {
     setAnchorEl(null);
   };
   return (
-    <div className="px-3  flex justify-between items-center w-full ">
-      <h1 className="font-bold cursor-pointer">
-        <NavLink to="/">
-          {" "}
-          <img src="/src/assets/imgs/logo.png" alt="logo" className="w-2/6" />
-        </NavLink>
-      </h1>
-      <ul className=" w-4/12 justify-between cursor-pointer hidden md:flex">
-        <li className="px-4 py-3 rounded-full  hover:bg-yellow-400 font-bold">
-          <NavLink
-            to="/rescue"
-            style={({ isActive }) => {
-              return {
-                fontWeight: isActive ? "bold" : "",
-              };
-            }}
-          >
-            貓咪救援
+    <>
+      <div
+        className={`px-2 flex justify-between items-center w-full fixed  z-50  ${
+          !onTop && "bg-white shadow-md"
+        }`}
+      >
+        <h1 className="font-bold cursor-pointer">
+          <NavLink to="/">
+            {" "}
+            <img
+              src="/src/assets/imgs/logo.png"
+              alt="logo"
+              className="w-2/6 rounded-full"
+            />
           </NavLink>
-        </li>
-        <li className="px-4 py-3  rounded-full hover:bg-yellow-400 font-bold">
-          <NavLink
-            to="/adopt"
-            style={({ isActive }) => {
-              return {
-                fontWeight: isActive ? "bold" : "",
-              };
-            }}
-          >
-            貓咪領養
-          </NavLink>
-        </li>
-        <li className="px-4 py-3 rounded-full  hover:bg-yellow-400 font-bold">
-          <div
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            其他用戶
-          </div>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem>
-              <NavLink to="/otherUsers?role=rescuer">救援者</NavLink>
-            </MenuItem>
-            <MenuItem>
-              <NavLink to="/otherUsers?role=adopter">認養者</NavLink>
-            </MenuItem>
-          </Menu>
-        </li>
-      </ul>
-      <div className="flex h-full hidden md:flex">
-        <div className="flex">
-          {currentUser && currentUser.role === "rescuer" && (
-            <>
-              <ModalSet btn={<CreateBtn />} form={<FormForRescue />} />
-              <ModalSet
-                btn={<EditBtn>新增收養</EditBtn>}
-                form={<FormForAdopt />}
-              />
-            </>
-          )}
-        </div>
-        <div className=" hover:shadow-md cursor-pointer rounded-full ml-10">
-          {currentUser ? (
-            <NavLink to={`/user/${currentUser.id}`}>
-              <Avatar>{getUserNameLastWord}</Avatar>
+        </h1>
+        <ul className=" w-4/12 justify-between cursor-pointer hidden md:flex">
+          <li className="px-4 py-3 rounded-full  hover:bg-yellow-400 font-bold">
+            <NavLink
+              to="/rescue"
+              style={({ isActive }) => {
+                return {
+                  fontWeight: isActive ? "bold" : "",
+                };
+              }}
+            >
+              貓咪救援
             </NavLink>
-          ) : (
-            <div className="flex items-center p-2">
-              <NavLink to="/signIn">
-                <AccountCircleIcon />
-                <span className="ml-3">登入</span>
-              </NavLink>
+          </li>
+          <li className="px-4 py-3  rounded-full hover:bg-yellow-400 font-bold">
+            <NavLink
+              to="/adopt"
+              style={({ isActive }) => {
+                return {
+                  fontWeight: isActive ? "bold" : "",
+                };
+              }}
+            >
+              貓咪領養
+            </NavLink>
+          </li>
+          <li className="px-4 py-3 rounded-full  hover:bg-yellow-400 font-bold">
+            <div
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              其他用戶
             </div>
-          )}
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem>
+                <NavLink to="/otherUsers?role=rescuer">救援者</NavLink>
+              </MenuItem>
+              <MenuItem>
+                <NavLink to="/otherUsers?role=adopter">認養者</NavLink>
+              </MenuItem>
+            </Menu>
+          </li>
+        </ul>
+        <div className="flex h-full hidden md:flex">
+          <div className="flex">
+            {currentUser && currentUser.role === "rescuer" && (
+              <>
+                <ModalSet btn={<CreateBtn />} form={<FormForRescue />} />
+                <ModalSet
+                  btn={<EditBtn>新增收養</EditBtn>}
+                  form={<FormForAdopt />}
+                />
+              </>
+            )}
+          </div>
+          <div className=" hover:shadow-md cursor-pointer rounded-full ml-10">
+            {currentUser ? (
+              <NavLink to={`/user/${currentUser.id}`}>
+                <Avatar>{getUserNameLastWord}</Avatar>
+              </NavLink>
+            ) : (
+              <div className="flex items-center p-2">
+                <NavLink to="/signIn">
+                  <AccountCircleIcon />
+                  <span className="ml-3">登入</span>
+                </NavLink>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center md:hidden">
+          <DrawerMenu />
         </div>
       </div>
-      <div className="flex items-center md:hidden">
-        <DrawerMenu />
-      </div>
-    </div>
+      <div className="h-24" style={{ backgroundColor: "#ffe9d2" }}></div>
+    </>
   );
 }
