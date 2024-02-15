ac40a4815534cb9d2f403cc45aca6575c9f76ce4
root <root@LAPTOP-LQRHI4AS>
2024-02-15 20:14:04 +0800

    feat: create menu in drawer

--- src/components/DrawerMenu.jsx
+++ src/components/DrawerMenu.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
-import Box from "@mui/material/Box";
+import { useState, useContext } from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext";
+
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import { NavLink } from "react-router-dom";
+import CastleIcon from "@mui/icons-material/Castle";
+import MedicationIcon from "@mui/icons-material/Medication";
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function TemporaryDrawer() {
+  //---取得登入者資料-----
+  const userData = useContext(CurrentUserContext);
+  const { currentUser } = userData;
+  //取得用戶名稱與路徑id
+  function getUserNameLastWord() {
+    if (currentUser) {
+      const userName = currentUser.name;
+      //用戶名稱只取最後一個字
+      const userNameLastWord = userName.substr(userName.length - 1);
+      return userNameLastWord;
+    }
+  }
+  //----處理drawer---------
   const [state, setState] = useState({
     right: false,
   });
@@ -28,25 +41,66 @@ export default function TemporaryDrawer() {
   };
 
   const list = (anchor) => (
-    <Box
-      sx={{ width: 250 }}
+    <div
+      className="w-60 px-4 py-3  h-full"
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
+      style={{ backgroundColor: "#ffdb7d" }}
     >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+      <div className="  cursor-pointer rounded-full  hover:translate-x-3 hover:scale-y-110 hover:text-yellow-800  hover:underline">
+        {currentUser ? (
+          <NavLink to={`/user/${currentUser.id}`}>
+            <div className="flex items-center">
+              <Avatar>{getUserNameLastWord}</Avatar>
+              <span className="ml-3 font-bold">{currentUser.name}</span>
+            </div>
+          </NavLink>
+        ) : (
+          <div className="flex items-center p-2">
+            <NavLink to="/signIn">
+              <AccountCircleIcon />
+              <span className="ml-3 font-bold">登入</span>
+            </NavLink>
+          </div>
+        )}
+      </div>
+
+      <ul className=" justify-between cursor-pointer  mt-5">
+        <li className=" py-3 rounded-full font-bold decoration-2  hover:translate-x-3 hover:scale-y-110 hover:text-yellow-800  hover:underline">
+          <NavLink to="/rescue">
+            <div className="flex items-center">
+              <MedicationIcon />
+              <span className="ml-3">貓咪募款</span>
+            </div>
+          </NavLink>
+        </li>
+        <li className=" py-3  rounded-full font-bold hover:translate-x-3 hover:scale-y-110 hover:text-yellow-800  hover:underline  ">
+          <NavLink to="/adopt">
+            <div className="flex items-center">
+              <CastleIcon />
+              <span className="ml-3">貓咪領養</span>
+            </div>
+          </NavLink>
+        </li>
+        <li className=" py-3  rounded-full  font-bold hover:translate-x-3 hover:scale-y-110 hover:text-yellow-800  hover:underline  ">
+          <NavLink to="/otherUsers?role=rescuer">
+            <div className="flex items-center">
+              <PersonIcon />
+              <span className="ml-3">其他救援者</span>
+            </div>
+          </NavLink>
+        </li>
+        <li className="py-3  rounded-full  font-bold hover:translate-x-3 hover:scale-y-110 hover:text-yellow-800  hover:underline ">
+          <NavLink to="/otherUsers?role=adopter">
+            <div className="flex items-center">
+              <PersonIcon />
+              <span className="ml-3"> 其他收養者</span>
+            </div>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 
   return (
