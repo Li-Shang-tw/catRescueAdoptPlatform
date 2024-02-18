4a47991490209409959f9b758948ad82a389d7ab
root <root@LAPTOP-LQRHI4AS>
2024-02-18 20:16:00 +0800

    feat: logout clean the currentUser and cookie and redirect to signIn

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,9 +1,24 @@
 import Button from "@mui/material/Button";
+import { useContext } from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext";
+import { useNavigate } from "react-router-dom";
 export default function LogOut() {
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  //使用useNavigate
+  const navigate = useNavigate();
+  function handleLogOut() {
+    // //清空currentUser
+    setCurrentUser(null);
+    //清空cookie
+    document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    alert("登出成功");
+    //導向登入頁面
+    navigate(`/signIn`);
+  }
   return (
     <div className="text-center">
       <h4 className="font-bold mb-5">你確定要登出嗎?</h4>
-      <Button variant="contained" color="error">
+      <Button variant="contained" color="error" onClick={handleLogOut}>
         <span className="font-semibold">登出</span>
       </Button>
     </div>
