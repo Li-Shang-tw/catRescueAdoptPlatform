1fcc7f2102ce79651ad3d79d1495c6eb540aab27
root <root@LAPTOP-LQRHI4AS>
2024-02-18 16:51:16 +0800

    fix: update the wayof  otherproject sort and pagination

--- src/components/OtherUserProjects.jsx
+++ src/components/OtherUserProjects.jsx
@@ -8,11 +8,11 @@ import NoData from "./NoData";
 export default function OtherUserProjects({ projectList, type, userId }) {
   //設定初始狀態
   const [projects, setProjects] = useState(null);
-  //current page
-  const [currentPage, setCurrentPage] = useState(1);
+
   //用useEffect來取得資料
   useEffect(() => {
     const fetchData = async () => {
+      //當處理收養者的收養的專案
       if (type === "pet") {
         const data = await getCatsOfAdoperAPI(userId);
         //當資料不存在時
@@ -23,6 +23,7 @@ export default function OtherUserProjects({ projectList, type, userId }) {
 
         setProjects(data);
       } else {
+        //當處理救援者的募資中、募資成功、送養中、已領養的專案
         const data = await getCatsOfOtherUserAPI(userId);
         //當資料不存在時
         if (data === "Not found") {
@@ -50,36 +51,25 @@ export default function OtherUserProjects({ projectList, type, userId }) {
       }
     };
     fetchData();
-  }, [projectList, type, userId]);
+  }, [type, userId]);
 
-  //當以載入的資料為空時，顯示暫無資料
+  //當以載入的資料為空時，顯示暫無pr資料
+  console.log(projects);
   if (!projects) {
     return <Loading />;
   } else if (projects === "Not found") {
     return <NoData />;
   }
 
-  //=========pagination=========
-  function handlePageChange(event, page) {
-    setCurrentPage(page);
-  }
-
-  //顯示當前頁面的資料
-  let currentRescueCats;
-  let totalPages;
-  if (projects) {
-    currentRescueCats = projects.slice((currentPage - 1) * 8, currentPage * 8);
-    //所有頁數
-    totalPages = Math.ceil(projects.length / 8);
+  function handleSetData(newValue) {
+    setProjects(newValue);
   }
 
   return (
     <>
       <OverView
-        currentCats={projects}
-        totalPages={totalPages}
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}
+        data={projects}
+        handleSetData={handleSetData}
         Card={
           ((type === "rescuing" || type === "rescued") && RescuingCardList) ||
           ((type === "adopting" || type === "adopted" || type === "pet") &&
