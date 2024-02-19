07d96ab3757d41dbb316d3c8bc383e1e8f0fb63f
root <root@LAPTOP-LQRHI4AS>
2024-02-19 13:59:35 +0800

    stye: tweak rwd of icerview banner

--- src/page/AdoptContainer.jsx
+++ src/page/AdoptContainer.jsx
@@ -28,15 +28,17 @@ export default function AdoptContainer() {
         Card={AdoptingCardList}
       >
         <Banner>
-          <div className="px-32 pt-12 pb-12 flex justify-between ">
+          <div className="py-12 w-10/12 mx-auto flex  flex-col items-center sm:flex-row  justify-between ">
             <div>
-              <h1 className="text-8xl font-bold text-gray-900 leading-tight mb-10">
+              <h1 className="font-bold text-gray-900 leading-tight mb-10 text-4xl md:text-7xl lg:text-8xl text-center">
                 貓咪領養總覽
               </h1>
-              <p className="mb-4 text-4xl">快帶可愛的貓貓回家</p>
+              <p className="mb-4 xl:text-4xl  md-text-3xl text-xl text-center">
+                快帶可愛的貓貓回家
+              </p>
             </div>
             <img
-              className="rounded-full  w-1/3"
+              className="rounded-full  w-3/5 sm:w-1/2 w-1/3"
               src="/src/assets/imgs/收養貓的圖示.jpg"
               alt="收養貓的圖示"
             />
--- src/page/RescueContainer.jsx
+++ src/page/RescueContainer.jsx
@@ -29,15 +29,17 @@ export default function RescueContainer() {
         sortType="rescue"
       >
         <Banner>
-          <div className="px-32 pt-12 pb-12 flex justify-between ">
+          <div className=" py-12 w-10/12 mx-auto flex  flex-col items-center sm:flex-row  justify-between ">
             <div>
-              <h1 className="text-8xl font-bold text-gray-900 leading-tight mb-10">
+              <h1 className="font-bold text-gray-900 leading-tight mb-10 text-4xl md:text-7xl lg:text-8xl text-center">
                 貓咪募款總覽
               </h1>
-              <p className="mb-4 text-4xl">受傷貓貓需要你的募款</p>
+              <p className="mb-4 xl:text-4xl  md-text-3xl text-xl text-center">
+                受傷貓貓需要你的募款
+              </p>
             </div>
             <img
-              className="rounded-full  w-1/3"
+              className="rounded-full  w-3/5 sm:w-1/2 md:w-1/3"
               src="/src/assets/imgs/貓咪募款圖.jpg"
               alt="募款貓的圖示"
             />
